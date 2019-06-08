ad5fc00134b0232af2459200b89b78e37eb4cb64
tien199013 <tien199013@localhost>
2019-06-08 11:18:17 +0100

    add slu structure

---
+++ dbUpTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DbUp;
+
+namespace dbUpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Data Source='C:\Users\zhao tian\Desktop\dbUpTest\SQLiteDB\DbUpTesting.db';";
+
+            var upgrader =
+                DeployChanges.To
+                    .SQLiteDatabase(connectionString)
+                    .WithScriptsAndCodeEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dbUpTest/Scripts/Script0001-CreateTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using DbUp.Engine;
+
+namespace dbUpTest.Scripts
+{
+    class Script0001_CreateTable : IScript
+    {
+        public string ProvideScript(Func<IDbCommand> dbCommandFactory)
+        {
+            using (var cmd = dbCommandFactory())
+            {
+                cmd.CommandText = "CREATE TABLE dbup_test( Name TEXT NOT NULL )";
+                cmd.ExecuteNonQuery();
+            }
+
+            return "";
+        }
+    }
+}
